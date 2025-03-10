cd2f8e68e6cf332cabb22d12c72ccf3bfabcbd24
Avik-creator <avikm744@gmail.com>
2025-03-10 16:55:36 +0530

    Added Analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-svh items-center justify-center`}
       >
+
         <ThemeProvider
         attribute="class"
         defaultTheme="system"
@@ -41,6 +43,7 @@ export default function RootLayout({
         {children}
         </ThemeProvider>
         <Toaster/>
+        <Analytics/>
       </body>
     </html>
   );
